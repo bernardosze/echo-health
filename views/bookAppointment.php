12845747e04746b66eada64d51a4f174ab338c2c
LoKimShJ <lokimshj@gmail.com>
2019-04-19 21:47:10 -0400

    Modified

--- views/bookAppointment.php
+++ views/bookAppointment.php
@@ -30,6 +30,8 @@ $doctor_id=$_GET['id'];
         <tr><td>Time</td><td><input type="time" name="time"></td><td>Date</td><td><input type="date" name="date"></td></tr>
     </table>
     <input type="submit" name="book" value="Book Appointment">
+
+
 </form>
 <?php
     if(isset($_POST['book'])){
@@ -37,11 +39,26 @@ $doctor_id=$_GET['id'];
         $bookTime=$_POST['time'];
         $time_convert = DateTime::createFromFormat( 'H:i',  $bookTime);
         $time_sql_format = $time_convert->format( 'H:i:s');
+        $checkquery="SELECT COUNT(*) FROM appointment where `FROM`= '$date $time_sql_format' and  doctor_id= $doctor_id;";
+        $count = $db->prepare($checkquery);
+        $count->execute();
+        $fetch=$count->fetch();
+        $result=$fetch[0];
+        $count->closeCursor();
 
-        $appointment_query = "INSERT INTO appointment(`FROM`,patient_id, doctor_id) VALUES ('$date $time_sql_format',1, $doctor_id)";
-        $statement2 = $db->prepare($appointment_query);
-        $statement2->execute();
-        $statement2->closeCursor();
+
+            if($result==0)
+            {
+                $appointment_query = "INSERT INTO appointment(`FROM`,patient_id, doctor_id) VALUES ('$date $time_sql_format',1, $doctor_id)";
+                $statement2 = $db->prepare($appointment_query);
+                $statement2->execute();
+                $statement2->closeCursor();
+                echo "Success";
+           }
+            else
+                {
+                    echo "This spot is not available";
+               }
     }
 
 ?>
